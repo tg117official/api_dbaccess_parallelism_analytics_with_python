62f9497521b7372d22a127d8e95815903f59468c
Sandeep Patil <sdp117@gmail.com>
2025-05-12 16:57:28 +0530

    SEP_ecom_completed

--- Session_2_python_requests_tour/project_2_ecom_or_profile_app/config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-from datetime import timedelta
-
-SECRET_KEY = 'supersecretkey'
-PERMANENT_SESSION_LIFETIME = timedelta(minutes=5)  # Auto-expire session after 5 minutes
--- Session_2_python_requests_tour/project_2_ecom_or_profile_app/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .auth import auth_bp
-from .dashboard import dashboard_bp
-from .profiles import profiles_bp
-from .products import products_bp
-from .cart import cart_bp
